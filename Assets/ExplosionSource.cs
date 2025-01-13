f09eb94a8e2b5442100691c52ef263ce21e6c7f7
AndreyShmykov <ashmikow@mail.com>
2025-01-13 14:26:29 +0400

    ColorRandomization

--- Assets/ExplosionSource.cs
+++ Assets/ExplosionSource.cs
@@ -21,8 +21,28 @@ public class ExplosionSource : MonoBehaviour
             var newZ = transform.position.z + Random.Range(-_power, _power);
             newObject.transform.position = new Vector3(newX, newY, newZ);
             newObject.transform.localScale /= 2;
+            SetRandomColor(newObject);
         }
 
         Destroy(gameObject);
+    }
+
+    private void SetRandomColor(GameObject targetObject)
+    {
+        Color randomColor = new Color(Random.value, Random.value, Random.value);
+
+        Renderer renderer = targetObject.GetComponent<Renderer>();
+
+        if (renderer != null)
+        {
+            renderer.material.color = randomColor;
+        }
+        else
+        {
+            SpriteRenderer spriteRenderer = targetObject.GetComponent<SpriteRenderer>();
+
+            if (spriteRenderer != null)
+                spriteRenderer.color = randomColor;
+        }
     }
 }
