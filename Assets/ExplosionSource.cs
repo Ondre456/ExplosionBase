e3b26942a1d57c232ebd105df7dd435ed1b54ccd
AndreyShmykov <ashmikow@mail.com>
2025-01-14 14:06:23 +0400

    Explosion correction

--- Assets/ExplosionSource.cs
+++ Assets/ExplosionSource.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplosionSource : MonoBehaviour
 {
-    [SerializeField] private float _explosionPower;
-
     private const int MinNumberOfNewObjects = 2;
     private const int MaxNumberOfNewObjects = 6;
-    private const float ExplosionPowerDividor = 2;
+    private const float ExplosionDividor = 2;
+
+    [SerializeField] private float _explosionPower;
 
     private float _chanceToExplode = 1f;
 
@@ -30,21 +28,21 @@ public class ExplosionSource : MonoBehaviour
 
         for (int i = 0; i < countOfNewObjects; i++)
         {
-            GameObject newObject = Instantiate(gameObject);
-            var newX = transform.position.x + Random.Range(-_explosionPower, _explosionPower);
-            var newY = transform.position.y + Random.Range(-_explosionPower, _explosionPower);
-            var newZ = transform.position.z + Random.Range(-_explosionPower, _explosionPower);
-            newObject.transform.position = new Vector3(newX, newY, newZ);
-            newObject.transform.localScale /= 2;
-            ExplosionSource newExplosionSource = newObject.GetComponent<ExplosionSource>();
-
-            if (newExplosionSource != null)
-                newExplosionSource._chanceToExplode = _chanceToExplode / ExplosionPowerDividor;
-            
-            Cube cube = newObject.GetComponent<Cube>();
-            cube?.SetRandomColor();
+            CreateExplosionProduct();
         }
     }
 
-   
+    private void CreateExplosionProduct()
+    {
+        GameObject explosionProduct = new GameObject("ExplosionProduct");
+        Cube cube = explosionProduct.AddComponent<Cube>();
+        cube.transform.localScale /= ExplosionDividor;
+        ExplosionSource newExplosionSource = explosionProduct.GetComponent<ExplosionSource>();
+
+        if (newExplosionSource != null)
+            newExplosionSource._chanceToExplode = _chanceToExplode / ExplosionDividor;
+
+        cube?.SetRandomColor();
+        cube?.AddRandomForce(_explosionPower);
+    }
 }
